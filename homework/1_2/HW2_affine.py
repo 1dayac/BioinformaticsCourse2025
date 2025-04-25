4fd5cd7d7e7e4db060c61026005adce81e5e6859
stacy-eliz <anastasiya.eliz@yandex.ru>
2025-04-26 00:31:07 +0300

    bugs fixed and tests added

--- homework/1_2/HW2_affine.py
+++ homework/1_2/HW2_affine.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import choice, randint
 from Bio.Align import substitution_matrices
 from Bio import pairwise2
 
@@ -18,19 +19,18 @@ def needleman_wunsch_affine(seq1, seq2):
 
     I_x = np.full((dim_i, dim_j), MIN)
     I_x[0, :] = gap_open + gap_extend * np.arange(dim_j)
-    I_x[0, 0] = 0
-    I_x[1:, :] = I_x[0, :]
+    I_x[0, :] += 1
     I_x[1:, 0] = MIN
 
     I_y = np.full((dim_i, dim_j), MIN)
     I_y[:, 0] = gap_open + gap_extend * np.arange(dim_i)
-    I_y[0, 0] = 0
-    I_y[:, 1:] = I_y[:, 0:1]
+    I_y[:, 0] += 1
     I_y[0, 1:] = MIN
 
     D = np.full((dim_i, dim_j), MIN)
     D[0, 0] = 0
-    D[1:, 1:] = 0
+    D[0, 1:] = I_x[0, 1:]
+    D[1:, 0] = I_y[1:, 0]
 
     for j in range(1, dim_j):
         for i in range(1, dim_i):
@@ -58,3 +58,44 @@ def needleman_wunsch_affine(seq1, seq2):
             j -= 1
 
     return align_seq1[::-1], align_seq2[::-1], D[-1][-1]
+
+
+def generate_sequences(min_len=2, max_len=10):
+    len = randint(min_len, max_len)
+    return ''.join(choice('ATGC') for _ in range(len))
+
+
+def test():
+    k_error = 0
+    sequences = [generate_sequences() for _ in range(10)]
+
+    for first_seq in range(len(sequences)):
+        for second_seq in range(first_seq + 1, len(sequences)):
+            s1, s2, score = needleman_wunsch_affine(sequences[first_seq], sequences[second_seq])
+
+            alignments = pairwise2.align.globalds(
+                sequences[first_seq],
+                sequences[second_seq],
+                matrix,
+                gap_open,
+                gap_extend
+            )
+
+            if alignments:
+                pairwise_alignment = alignments[0]
+                pairwise_score = pairwise_alignment.score
+                pairwise_seq1 = pairwise_alignment.seqA
+                pairwise_seq2 = pairwise_alignment.seqB
+
+                if score != pairwise_score:
+                    print(f"Исходные последовательности: {sequences[first_seq]}, {sequences[second_seq]}")
+                    print(f"Score: {score}")
+                    print(f"{s1}\n{s2}")
+                    print(f"Bio.pairwise2:\n{pairwise_seq1}\n{pairwise_seq2}")
+                    print(f"Bio.pairwise2 score: {pairwise_score}")
+                    print('__________')
+                    k_error += 1
+    return k_error
+
+
+print(test())
--- homework/1_2/HW2_needleman_wunsch.py
+++ homework/1_2/HW2_needleman_wunsch.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import choice, randint
 from Bio import pairwise2
 from Bio.Align import substitution_matrices
 
@@ -40,4 +41,45 @@ def needleman_wunsch(seq1, seq2):
             align_seq2 += seq2[j - 1]
             j -= 1
 
-    return align_seq1, align_seq2, D[-1][-1]
+    return align_seq1[::-1], align_seq2[::-1], D[-1][-1]
+
+
+def generate_sequences(min_len=2, max_len=10):
+    len = randint(min_len, max_len)
+    return ''.join(choice('ATGC') for _ in range(len))
+
+
+def test():
+    k_error = 0
+    sequences = [generate_sequences() for _ in range(10)]
+
+    for first_seq in range(len(sequences)):
+        for second_seq in range(first_seq + 1, len(sequences)):
+            s1, s2, score = needleman_wunsch(sequences[first_seq], sequences[second_seq])
+
+            alignments = pairwise2.align.globalds(
+                sequences[first_seq],
+                sequences[second_seq],
+                matrix,
+                gap,
+                gap
+            )
+
+            if alignments:
+                pairwise_alignment = alignments[0]
+                pairwise_score = pairwise_alignment.score
+                pairwise_seq1 = pairwise_alignment.seqA
+                pairwise_seq2 = pairwise_alignment.seqB
+
+                if score != pairwise_score:
+                    print(f"Исходные последовательности: {sequences[first_seq]}, {sequences[second_seq]}")
+                    print(f"Score: {score}")
+                    print(f"{s1}\n{s2}")
+                    print(f"Bio.pairwise2:\n{pairwise_seq1}\n{pairwise_seq2}")
+                    print(f"Bio.pairwise2 score: {pairwise_score}")
+                    print('__________')
+                    k_error += 1
+    return k_error
+
+
+print(test())
